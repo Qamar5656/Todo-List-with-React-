f342336f4ba5cce3c29620471b89820936b4bb5c
Qamar5656 <qamar4157@gmail.com>
2025-08-04 20:44:47 +0500

    adding guess the word game component

---
+++ src/Components/GuessTheWord.jsx
@@ -0,0 +1,11 @@
+// Filter todos by search query
+// const filteredTodos = todos.filter((todo) =>
+//   todo.text.toLowerCase().includes(searchQuery.toLowerCase())
+// );
+import React from "react";
+
+const GuessTheWord = () => {
+  return <div>GuessTheWord</div>;
+};
+
+export default GuessTheWord;
