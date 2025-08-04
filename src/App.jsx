be717fe698eb8bfff29de54bbc8e3283e5944a41
Qamar5656 <qamar4157@gmail.com>
2025-08-04 20:44:18 +0500

    adding routing in website

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
 import TodoApp from "./Components/Todo";
+import GuessTheWord from "./Components/GuessTheWord";
+import About from "./Components/About";
+import TestComp from "./Components/TestComp";
 
 function App() {
   return (
     <>
-      <div>
-        <div>
-          <TodoApp />
-        </div>
-      </div>
+      <TodoApp />
+      {/* <TestComp /> */}
     </>
+
+    // <BrowserRouter>
+    //   <Navbar />
+
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/about" element={<About />} />
+    //     <Route path="/services" element={<About />} />
+    //     <Route path="/blog" element={<About />} />
+    //     {/* Optional: add other components */}
+    //     {/* <Route path="/todo" element={<TodoApp />} /> */}
+    //     {/* <Route path="/game" element={<GuessTheWord />} /> */}
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
